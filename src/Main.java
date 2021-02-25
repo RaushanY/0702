53c9f8870f77682ff0da8ba4b329cc3e15be8d7e
RaushanY <raushan4066@gmail.com>
2021-02-25 20:37:15 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("3 * 1000 / 100 + 1000");
+        double rate = 3;
+        double sum = 1000;
+        double percent = 100;
+        double investment = 1000;
+       double result = 3 * 1000 / 100 + 1000;
+        System.out.println(result);}
+}
