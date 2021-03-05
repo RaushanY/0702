a59e128ebbc7d336cee254681b36e212829d5e85
RaushanY <raushan4066@gmail.com>
2021-03-05 20:03:01 +0300

    fixed #2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("3 * 1000 / 100 + 1000");
         double rate = 3;
         double sum = 1000;
         double percent = 100;
         double investment = 1000;
-       double result = 3 * 1000 / 100 + 1000;
+       double result = rate * sum / percent + investment;
         System.out.println(result);}
 }
